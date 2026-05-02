60d74fc9a038ffedca76c5f59f436197587af5a3
luckuhack_08 <lakshit9602@gmail.com>
2026-05-02 10:41:39 +0530

    fghjk

---
+++ crt12.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int i;
+    cin>>i;
+
+    int n;
+    cout<<"Enter any number";
+    cin>>n;
+    while(i!= n){
+        if(i%2==0){
+            cout<<i<<endl;
+        }
+i++;
+
+    }
+    return 0;
+}
