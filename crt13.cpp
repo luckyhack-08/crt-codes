86c7f49ced433e032e05d864b4aaf355e69e9328
luckuhack_08 <lakshit9602@gmail.com>
2026-05-04 09:30:38 +0530

    day 4

---
+++ crt13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"enter value of n"<<endl;
+cin>>n;
+int x=0;
+int y=1;
+for(int i=1; i<=n; i++){
+cout<<x<<" ";
+int z=x+y;
+x=y;
+y=z;
+}
+return 0;
+}
---
+++ crt14.cpp
@@ -0,0 +1,19 @@
+//print the pattern
+//****
+//****
+//****
+//**** 
+//**** */ */ */ */
+
+#include<iostream>
+using namespace std;
+int main (){
+    int row=5,column=4;
+    for (int i=1;i<=5;i++){
+        for (int j=1;j<=4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+return 0;
+}
