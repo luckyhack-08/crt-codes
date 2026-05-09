cf1d945843dfe97877a2e9934f30c8618b1297ac
luckuhack_08 <lakshit9602@gmail.com>
2026-05-09 12:08:55 +0530

    Day 9

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[5];
+   int size =sizeof(arr)/sizeof(arr[0]);
+   for (int i=0;i<size;i++){
+    cin>>arr[i];
+   }
+   for (int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+   }
+   return 0;
+}
